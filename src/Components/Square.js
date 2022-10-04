ecacbc412ec7bd3b2fa0a23671de71a3cab2b876
JICaddick <james_caddick16@hotmail.com>
2022-10-04 12:57:19 -0500

    reverse engineering time

---
+++ src/Components/Square.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import '/Users/learnacademy/Desktop/Personal Projects/co-pilot-tic-tac-toe/src/App.css'
+
+class Square extends Component {
+
+handleClick = () => {
+  this.props.handleGamePlay(this.props.index)
+
+}
+
+  render() {
+    return(
+      <>
+        <div className ="square" onClick = {this.handleClick}> <h1>{this.props.value}</h1></div>
+      </>
+    )
+  }
+}
+export default Square
